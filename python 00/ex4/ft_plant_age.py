b6fc0383825f581ca96557602e6bbb366bfc364e
Kevin Le Scornet <kle-scor@c1r2p4.42nice.fr>
2026-03-25 12:48:28 +0100

    correc

--- python 00/ex4/ft_plant_age.py
+++ python 00/ex4/ft_plant_age.py
@@ -1,6 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_plant_age.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kle-scor <kle-scor@student.42nice.fr>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/25 11:34:22 by kle-scor          #+#    #+#              #
+#    Updated: 2026/03/25 11:34:25 by kle-scor         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 def ft_plant_age():
     x = int(input("Enter plant age in days: "))
-    if x >= 60:
+    if x > 60:
         print("Plant is ready to harvest!")
-    if x < 60:
+    if x <= 60:
         print("Plant needs more time to grow.")
--- python 00/ex5/ft_water_reminder.py
+++ python 00/ex5/ft_water_reminder.py
@@ -1,6 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_water_reminder.py                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kle-scor <kle-scor@student.42nice.fr>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/25 11:34:31 by kle-scor          #+#    #+#              #
+#    Updated: 2026/03/25 11:34:34 by kle-scor         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 def ft_water_reminder():
     x = int(input("Days since last watering: "))
-    if x >= 2:
+    if x > 2:
         print("Water the plants!")
-    if x < 2:
+    if x <= 2:
         print("Plants are fine")
