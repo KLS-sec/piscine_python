9cfc394e342b4eecd3ffaf8b76face1a2a1d0a83
Kevin Le Scornet <kle-scor@c1r2p4.42nice.fr>
2026-03-25 18:14:36 +0100

    save soir

---
+++ python 01/ex2/ft_plant_growth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# if __name__ == "__main__":
+
+# print(), range(), round()
+
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+print("=== Garden Plant Growth ===")
+p1 = Plant("Rose", 25.0, 30)
+p1.show()
+
+"""
+• Reuse your Plant class from Exercise 1
+FAIT
+
+• Plants need to be able to grow() and age() on their own (i.e., these will be methods
+of the class)
+
+• Simulate a week of growth for a plant, then access the data in the class to get the
+final height and display the total week increase.
+
+• Consider how the plant should change over time when using grow() and age().
+Different plants can evolve differently.
+"""
