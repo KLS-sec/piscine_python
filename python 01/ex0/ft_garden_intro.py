aaadd675c19c5fd3cd04ca5e20fe52e37d9e2972
Kevin Le Scornet <kle-scor@c1r2p4.42nice.fr>
2026-03-25 17:59:29 +0100

    01

---
+++ python 01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+name = "Rose"
+height = 25
+age = 30
+
+if __name__ == "__main__":
+    print("=== Welcome to My {} ===".format("Garden"))
+    print("Plant: ", name)
+    print(f"Height: {height}cm")
+    print("Age: {} days".format(age))
+    print("=== End of Program ===")
---
+++ python 01/ex1/ft_garden_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# if __name__ == "__main__":
+
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+print("=== Garden Plant Registry ===")
+p1 = Plant("Rose", 25, 30)
+p1.show()
+p1 = Plant("Sunflower", 80, 45)
+p1.show()
+p1 = Plant("Cactus", 15, 120)
+p1.show()
