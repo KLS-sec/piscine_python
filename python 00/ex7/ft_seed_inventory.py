d0b6a48146703fc8c8b3c5c37b68ef3611b6a779
Kevin Le Scornet <kle-scor@c1r2p4.42nice.fr>
2026-03-25 13:00:23 +0100

    correc

--- python 00/ex7/ft_seed_inventory.py
+++ python 00/ex7/ft_seed_inventory.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_seed_inventory.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: kle-scor <kle-scor@student.42nice.fr>      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/03/25 11:35:03 by kle-scor          #+#    #+#              #
-#    Updated: 2026/03/25 11:35:06 by kle-scor         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if (unit == "packets"):
         unit = "packets available"
