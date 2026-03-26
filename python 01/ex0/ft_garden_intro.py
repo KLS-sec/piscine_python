d29440d3b22d6cee3a1d1f21fdb9847d43389555
kle-scor <lescornetkevin@gmail.com>
2026-03-26 17:50:09 +0100

    save soir

--- python 01/ex0/ft_garden_intro.py
+++ python 01/ex0/ft_garden_intro.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+def main() -> None:
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print("Plant: ", name)
+    print("Height: ", height, "cm")
+    print("Age: ", age, " days")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
+
+"""
 name = "Rose"
 height = 25
 age = 30
@@ -10,3 +25,4 @@ if __name__ == "__main__":
     print(f"Height: {height}cm")
     print("Age: {} days".format(age))
     print("=== End of Program ===")
+"""
--- python 01/ex2/ft_plant_growth.py
+++ python 01/ex2/ft_plant_growth.py
@@ -3,29 +3,38 @@
 
 # print(), range(), round()
 
-class Plant:
-    def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
+def main() -> None:
+    class Plant:
+        def __init__(self, name, height, age):
+            self.name = name
+            self.height = height
+            self.age = age
 
-    def show(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
+        def show(self):
+            print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+    print("=== Garden Plant Registry ===")
+    p1 = Plant("Rose", 25, 30)
+    p1.show()
+    p1 = Plant("Sunflower", 80, 45)
+    p1.show()
+    p1 = Plant("Cactus", 15, 120)
+    p1.show()
 
 
-print("=== Garden Plant Growth ===")
-p1 = Plant("Rose", 25.0, 30)
-p1.show()
+if __name__ == "__main__":
+    main()
+
 
 """
 • Reuse your Plant class from Exercise 1
 FAIT
 
-• Plants need to be able to grow() and age() on their own (i.e., these will be methods
-of the class)
+• Plants need to be able to grow() and age() on their own (i.e., these
+will be methods of the class)
 
-• Simulate a week of growth for a plant, then access the data in the class to get the
-final height and display the total week increase.
+• Simulate a week of growth for a plant, then access the data in the
+class to get the final height and display the total week increase.
 
 • Consider how the plant should change over time when using grow() and age().
 Different plants can evolve differently.
