072d9fc8b3735c7e45c97fc4bf102e266b422752
kle-scor <lescornetkevin@gmail.com>
2026-04-08 12:04:42 +0200

    save

--- python 02/ex0/ft_first_exception.py
+++ python 02/ex0/ft_first_exception.py
@@ -7,23 +7,19 @@ def input_temperature(temp_str: str) -> int:
 def test_temperature() -> None:
     value: list[str] = ["25", "abc"]
     for n in value:
-        print("Input data is '", n, "'", sep="")
+        print("\nInput data is '", n, "'", sep="")
         try:
             x: int = input_temperature(n)
-            print("Temperature is now ", x, "°C\n", sep="")
+            print("Temperature is now ", x, "°C", sep="")
         except Exception as err:
             print("Caught input_temperature error:", err, "\n")
 
 
 def main() -> None:
-    print("=== Garden Temperature ===\n")
+    print("=== Garden Temperature ===")
     test_temperature()
     print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
     main()
-
-"""
-checked with gpt
-"""
--- python 02/ex1/ft_raise_exception.py
+++ python 02/ex1/ft_raise_exception.py
@@ -12,23 +12,19 @@ def input_temperature(temp_str: str) -> int:
 def test_temperature() -> None:
     value: list[str] = ["25", "abc", "100", "-50"]
     for n in value:
-        print("Input data is '", n, "'", sep="")
+        print("\nInput data is '", n, "'", sep="")
         try:
             x: int = input_temperature(n)
-            print("Temperature is now ", x, "°C\n", sep="")
+            print("Temperature is now ", x, "°C", sep="")
         except Exception as err:
-            print("Caught input_temperature error:", err, "\n")
+            print("Caught input_temperature error:", err)
 
 
 def main() -> None:
-    print("=== Garden Temperature Checker ===\n")
+    print("=== Garden Temperature Checker ===")
     test_temperature()
-    print("All tests completed - program didn't crash!")
+    print("\nAll tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
     main()
-
-"""
-checked with gpt
-"""
