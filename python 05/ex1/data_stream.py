1e86844ce1ad9b8a50dbc605bd6573bb754f99c7
kle-scor <lescornetkevin@gmail.com>
2026-04-16 17:13:15 +0200

    sqve soir

---
+++ python 05/ex1/data_stream.py
@@ -0,0 +1,3 @@
+https://www.w3schools.com/python/python_polymorphism.asp
+https://www.geeksforgeeks.org/python/polymorphism-in-python/
+https://realpython.com/ref/glossary/polymorphism/#:~:text=Polymorphism%20enables%20you%20to%20use,through%20inheritance%20and%20method%20overriding.
